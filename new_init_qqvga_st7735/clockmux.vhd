659ba8f87a020ef55656b30374cd9cf15a1a7022
kedziorno <kjoblak85@gmail.com>
2022-08-16 16:07:57 +0200

    rewrite clockmux with generic N

--- new_init_qqvga_st7735/clockmux.vhd
+++ new_init_qqvga_st7735/clockmux.vhd
@@ -1,64 +1,52 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:34:24 08/16/2022 
+-- Design Name: 
+-- Module Name:    clockmux - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
 -- This design is not completed. It is left as an exercise for
 -- the reader to complete this clock multiplexer.
 -- from VHDL Language Kevin Skahill CH 6 REPEATER - CLOCKMUX
 
-library ieee;
-use ieee.std_logic_1164.all;
 library UNISIM;
 use UNISIM.VComponents.all;
 
-entity clockmux is port (
-	areset           : in std_logic;        -- Asynch Reset
-	clk1             : in std_logic;        -- Clock 1
-	clk2             : in std_logic;        -- Clock 2
-	clk3             : in std_logic;        -- Clock 3
-	clk4             : in std_logic;        -- Clock 4
-	clk5             : in std_logic;        -- Clock 5
-	clk6             : in std_logic;        -- Clock 6
-	clk7             : in std_logic;        -- Clock 7
-	clk8             : in std_logic;        -- Clock 8
-	clk9             : in std_logic;        -- Clock 9
-	sel1             : in std_logic;        -- Clock Select 1
-	sel2             : in std_logic;        -- Clock Select 2
-	sel3             : in std_logic;        -- Clock Select 3
-	sel4             : in std_logic;        -- Clock Select 4
-	sel5             : in std_logic;        -- Clock Select 5
-	sel6             : in std_logic;        -- Clock Select 6
-	sel7             : in std_logic;        -- Clock Select 7
-	sel8             : in std_logic;        -- Clock Select 8
-	sel9             : in std_logic;        -- Clock Select 9
-	rxclk            : buffer std_logic);      -- RX Clock
+entity clockmux is
+generic (N : integer := 4);
+port (
+	i_reset : in std_logic; -- Asynch Resets
+	i_clk : in std_logic_vector(N-1 downto 0); -- Clock 1-N
+	i_sel : in std_logic_vector(N-1 downto 0); -- Clock Select Mux N
+	o_clk : buffer std_logic -- out
+);
 end clockmux;
 
 architecture a1 of clockmux is
-	signal t : std_logic_vector(8 downto 0);
-	signal o : std_logic;
-begin
-	p0 : process (areset) is
-	begin
-		if (areset = '1') then
-			rxclk <= '0';
-		else
-			rxclk <= o;
-		end if;
-	end process p0;
-
-	t <= sel1 & sel2 & sel3 & sel4 & sel5 & sel6 & sel7 & sel8 & sel9;
-
-	with t select o <=
-	clk1 when "000000001",
-	clk2 when "000000010",
-	clk3 when "000000100",
-	clk4 when "000001000",
-	clk5 when "000010000",
-	clk6 when "000100000",
-	clk7 when "001000000",
-	clk8 when "010000000",
-	clk9 when "100000000",
-	'X' when others;
-end architecture a1;
-
-architecture a2 of clockmux is
 
 component rdff1b is port (
 clk, reset: in std_logic;
@@ -68,51 +56,123 @@ qb: buffer std_logic);
 end component rdff1b;
 for all : rdff1b use entity work.rdff1b(archrdff1b);
 
-signal clockp,clockn,t1,t2,t3,andout,sel : std_logic_vector(8 downto 0);
+signal clockp,clockn,t1,t2,t3,andout,sel : std_logic_vector(N-1 downto 0);
+
+signal vor : std_logic_vector(N-1 downto 0) := (others => '0');
+--signal vand : std_logic := '1';
+
+type ta is array (0 to N-1) of std_logic_vector(N-1 downto 0);
+signal a : ta;
 
 begin
 
---g0 : for i in 0 to 8 generate
---	u0 : rdff1b port map (clk => clockp(i), reset => areset, d => sel(i), q => t1(i), qb => open);
---	u1 : rdff1b port map (clk => clockn(i), reset => areset, d => t1(i), q => t2(i), qb => t3(i));
+--vor <= '0' when i_reset = '1' else vor;
+--vand <= '1' when i_reset = '1' else vand;
+
+--g0 : for i in 0 to N-1 generate
+--	u0 : rdff1b port map (clk => clockp(i), reset => i_reset, d => sel(i), q => t1(i), qb => open);
+--	u1 : rdff1b port map (clk => clockn(i), reset => i_reset, d => t1(i), q => t2(i), qb => t3(i));
 --	u2 : AND2 port map (I0 => t2(i), I1 => clockp(i), O => andout(i));
 --end generate g0;
 
-g0 : for i in 0 to 8 generate
-	u0 : rdff1b port map (clk => clockp(i), reset => areset, d => sel(i), q => t1(i), qb => t3(i));
+g0 : for i in 0 to N-1 generate
+	u0 : rdff1b port map (clk => clockp(i), reset => i_reset, d => sel(i), q => t1(i), qb => t3(i));
 	u2 : AND2 port map (I0 => t1(i), I1 => clockp(i), O => andout(i));
 end generate g0;
 
-sel(0) <= sel1 and t3(1) and t3(2) and t3(3) and t3(4) and t3(5) and t3(6) and t3(7) and t3(8);
-sel(1) <= t3(0) and sel2 and t3(2) and t3(3) and t3(4) and t3(5) and t3(6) and t3(7) and t3(8);
-sel(2) <= t3(0) and t3(1) and sel3 and t3(3) and t3(4) and t3(5) and t3(6) and t3(7) and t3(8);
-sel(3) <= t3(0) and t3(1) and t3(2) and sel4 and t3(4) and t3(5) and t3(6) and t3(7) and t3(8);
-sel(4) <= t3(0) and t3(1) and t3(2) and t3(3) and sel5 and t3(5) and t3(6) and t3(7) and t3(8);
-sel(5) <= t3(0) and t3(1) and t3(2) and t3(3) and t3(4) and sel6 and t3(6) and t3(7) and t3(8);
-sel(6) <= t3(0) and t3(1) and t3(2) and t3(3) and t3(4) and t3(5) and sel7 and t3(7) and t3(8);
-sel(7) <= t3(0) and t3(1) and t3(2) and t3(3) and t3(4) and t3(5) and t3(6) and sel8 and t3(8);
-sel(8) <= t3(0) and t3(1) and t3(2) and t3(3) and t3(4) and t3(5) and t3(6) and t3(7) and sel9;
+--sel(0) <= i_sel(0) and t3(1) and t3(2) and t3(3);
+--sel(1) <= t3(0) and i_sel(1) and t3(2) and t3(3);
+--sel(2) <= t3(0) and t3(1) and i_sel(2) and t3(3);
+--sel(3) <= t3(0) and t3(1) and t3(2) and i_sel(3);
 
-rxclk <= andout(0) or andout(1) or andout(2) or andout(3) or andout(4) or andout(5) or andout(6) or andout(7) or andout(8);
+--g5 : for i in 0 to N-1 generate
+--	g5a : for j in 0 to N-1 generate
+--		g5b : if (i=j) generate
+--		a(i)(j) <= '1';
+--		end generate g5b;
+--		g5c : if (i/=j) generate
+--			a(i)(j) <= '0';
+--		end generate g5c;
+--	end generate g5a;
+--end generate g5;
 
-clockp(0) <= clk1;
-clockp(1) <= clk2;
-clockp(2) <= clk3;
-clockp(3) <= clk4;
-clockp(4) <= clk5;
-clockp(5) <= clk6;
-clockp(6) <= clk7;
-clockp(7) <= clk8;
-clockp(8) <= clk9;
+g2 : process (i_reset,i_sel,t3,a) is
+variable vand : std_logic;
+begin
+	if (i_reset = '1') then
+	vand := '1';
+	sel <= (others => '0');
+	else
+	l0 : for i in 0 to N-1 loop
+		vand := '1';
+	l1 : for j in 0 to N-1 loop
+	if (i=j) then
+--		a1 : AND2 port map (I0 => vand, I1 => i_sel(i), O => vand);
+		a1 : vand := i_sel(i) and vand;
+	elsif (i/=j) then
+--		a2 : AND2 port map (I0 => vand, I1 => t3(i), O => vand);
+		a2 : vand := t3(i) and vand;
+	else
+		a3 : vand := '1';
+	end if;
+	end loop l1;
+	sel(i) <= vand;
+	end loop l0;
+	end if;
+end process g2;
 
-clockn(0) <= not clk1;
-clockn(1) <= not clk2;
-clockn(2) <= not clk3;
-clockn(3) <= not clk4;
-clockn(4) <= not clk5;
-clockn(5) <= not clk6;
-clockn(6) <= not clk7;
-clockn(7) <= not clk8;
-clockn(8) <= not clk9;
+--g2 : for i in 0 to N-1 generate
+--g2a : for j in 0 to N-1 generate
+--begin
+--	g2b : if (a(i)(j)='1') generate
+--		g2bb : AND2 port map (I0 => vand, I1 => i_sel(i), O => vand);
+--	end generate g2b;
+--	g2c : if (a(i)(j)='0') generate
+--		g2cc : AND2 port map (I0 => vand, I1 => t3(i), O => vand);
+--	end generate g2c;
+--	sel(i) <= vand;
+--end generate g2a;
+--end generate g2;
 
-end architecture a2;
+g1 : process (i_reset,andout) is
+	variable vor : std_logic;
+begin
+	if (i_reset = '1') then
+		vor := '0';
+		o_clk <= '0';
+	else
+		vor := '0';
+		l0 : for i in 0 to N-1 loop
+			vor := andout(i) or vor;
+		end loop l0;
+	end if;
+	o_clk <= vor;
+end process g1;
+
+--g1 : for i in 0 to N-1 generate
+----	shared variable v : std_logic_vector(N-1 downto 0) := (others => '0');
+--begin
+--	g1c : if (i=0) generate
+--		vor(i) <= andout(i);
+----		g1aa : OR2 port map (I0 => vor(i), I1 => andout(i), O => vor(i));
+--	end generate g1c;
+--	g1a : if (i>0 and i<N-1) generate
+----		g1aa : OR2 port map (I0 => vor(i-1), I1 => andout(i), O => vor(i));
+--		vor(i) <= andout(i) or vor(i-1);
+--	end generate g1a;
+--	g1b : if (i=N-1) generate
+----		g1bb : OR2 port map (I0 => vor(i-1), I1 => andout(i), O => vor(i));
+--			vor(i) <= andout(i) or vor(i-1);
+--			o_clk <= vor(i);
+--	end generate g1b;
+--end generate g1;
+
+g3 : for i in 0 to N-1 generate
+	g3a : clockp(i) <= i_clk(i);
+end generate g3;
+
+g4 : for i in 0 to N-1 generate
+	g4a : clockn(i) <= not i_clk(i);
+end generate g4;
+
+end architecture a1;
