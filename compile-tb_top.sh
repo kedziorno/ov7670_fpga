bfc145136883e546f789e1732fe4293b4aaca844
kedziorno <kjoblak85@gmail.com>
2026-03-11 17:11:33 +0100

    fix compile sh

--- compile-tb_top.sh
+++ compile-tb_top.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
-#. $HOME/.local/Xilinx/14.7/ISE_DS/settings64.sh
-#fuse -v -intstyle ise -incremental -lib unisims_ver -lib unimacro_ver -lib xilinxcorelib_ver \
-#-o tb_ov7670_camera_emulator_isim_beh.exe \
-#-prj tb_ov7670_camera_emulator_beh.prj \
-#work.tb_ov7670_camera_emulator \
-#work.glbl
-#if [ $? = 0 ]; then
-#./tb_ov7670_camera_emulator_isim_beh.exe -intstyle ise -tclbatch isim.cmd \
-#-view tb_ov7670_camera_emulator.wcfg \
-#-wdb tb_ov7670_camera_emulator_isim_beh.wdb \
-#-log isim_output.txt
-#fi
-
-#!/bin/bash
+# Usage script.sh "y|n" [DB WDB] "time" [numeric] "period" [ms|us|ns]
 source /home/user/.local/Xilinx/14.7/ISE_DS/settings64.sh
-top=$1
-time_period=$2
-time_scale=$3
+database=$1
+top=$2
+time_period=$3
+time_scale=$4
 rm -rf isim_output.txt
 rm -rf isim_gui.cmd
 echo "onerror {resume}" > isim_gui.cmd
+if [ x${database} = x"y" ]; then
 grep "wvobject fp_name=\"/" tb_${top}.wcfg | sed -E "s/<wvobject fp_name=\"(.*)\" type=\"(.*)\" db_ref_id=\"(.*)\">/\1/g" | awk -F " " '{printf ("wave add %s\n", $0)}' >> isim_gui.cmd
+fi
 echo "run ${time_period} ${time_scale}" >> isim_gui.cmd
 echo "quit -f" >> isim_gui.cmd
 fuse -v -intstyle ise \
@@ -28,8 +18,13 @@ fuse -v -intstyle ise \
 -prj tb_${top}_beh.prj \
 work.tb_${top}
 if [ $? = 0 ]; then
+if [ x${database} = x"y" ]; then
 ./tb_${top}_isim_beh.exe -intstyle ise -tclbatch isim_gui.cmd \
 -view tb_${top}.wcfg \
 -wdb tb_${top}_isim_beh.wdb \
 -log isim_output.txt
+else
+./tb_${top}_isim_beh.exe -intstyle ise -tclbatch isim_gui.cmd \
+-log isim_output.txt
+fi
 fi
