c6fbd59dd5fe2c89d7e4274c35ca78b192ee4b1f
kedziorno <kjoblak85@gmail.com>
2026-04-12 11:00:08 +0200

    probe 2 - almost stable image / slide 3 v120pix h128pix, better adjust

--- Top.vhd
+++ Top.vhd
@@ -810,7 +810,8 @@ begin
 ----          if (vga_hsync_i_prev = '0' and vga_hsync_i = '1') then
 ----          end if;
 --        end if;
-        if (ov7670_vs_next = "00" and vga_vsync_sig = '0') then
+--        if (ov7670_vs_next = "01" and vga_vsync_sig = '0') then
+        if (ov7670_vs_next = "11" and ov7670_vs = '0') then
           p1_state <= a0a;
         end if;
       when a0a =>
