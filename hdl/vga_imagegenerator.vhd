18301995867e3a088b37e8419fc6ee3dd084b544
kedziorno <kjoblak85@gmail.com>
2026-05-03 23:11:24 +0200

    probe 4 - rgb565 24mhz

--- hdl/vga_imagegenerator.vhd
+++ hdl/vga_imagegenerator.vhd
@@ -32,7 +32,7 @@ begin
 --end if;
 --end if;
 --end process;
-RGB_out <= Data_in1(15 downto 13) & Data_in1(11 downto 9) & Data_in1(7 downto 6) when active_area1 = '1' else (others => '0');
+RGB_out <= Data_in1(15 downto 13) & Data_in1(10 downto 8) & Data_in1(4 downto 3) when active_area1 = '1' else (others => '0');
 --else (others => '0');
 --RGB_out <= Data_in1(7 downto 0) when active_area1 = '1' else (others => '0');
 end Behavioral;
