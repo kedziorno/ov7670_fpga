8d166c890fd40476632ac973e17c6ce0d2955ac6
kedziorno <kjoblak85@gmail.com>
2026-05-04 11:52:51 +0200

    probe 1 - regs comment

--- hdl/ov7670_rom.vhd
+++ hdl/ov7670_rom.vhd
@@ -10,48 +10,48 @@ package p_ov7670_rom is
 x"1280", -- COM7   Reset -- Do it twice to make sure its wiped
 x"1280", -- COM7   Reset -- choose output format. 
 x"fffe",
-x"1204", -- scaling
-x"11"&"10000000",
-x"6b"&"00000000",
-x"8c02",
-x"0c00", -- scaling
-x"3e00", -- scaling
-x"0400",
-x"40f8", -- COM15
-x"138f",
-x"4108",
-x"3a04",
-x"1418",
-x"4fb3",
-x"1500",
-x"50b3",
-x"5100",
-x"523d",
-x"53a7",
-x"54e4",
-x"589e",
-x"3dc0",
-x"1714",
-x"1802",
-x"3280",
-x"1903",
-x"1a7b",
-x"030a",
-x"0f41",
-x"1e00",
-x"330b",
-x"3c40",
-x"6900",
-x"7400",
-x"703a", -- ba - pattern
-x"7135",
-x"7222",
-x"73f2",
-x"a202",
-x"b084",
-x"b10c",
-x"b20e",
-x"b380",
+x"12"&"00000100", -- COM7 2 - RGB
+x"11"&"10000000", -- CLKRC 7 - reserved
+x"6b"&"00000000", -- DBLV
+x"8c"&"00000010", -- RGB444 xRGB
+x"0c"&"00000000", -- COM3
+x"3e"&"00000000", -- COM14
+x"04"&"00000000", -- COM1
+x"40"&"11111000", -- COM15 76 - FF, 54 - RGB555
+x"13"&"0001ffff", -- COM8 43 - reserved, 2 - AGC, 1 - AWB, 0 - AEC
+x"41"&"00001000", -- COM16 3 - AWB gain
+x"3a"&"00000100", -- TSLB 2 - reserved
+x"14"&"00011000", -- COM9 4 - AGC 4x, 3 - reserved
+x"4fb3", -- MTX1
+x"50b3", -- MTX2
+x"5100", -- MTX3
+x"523d", -- MTX4
+x"53a7", -- MTX5
+x"54e4", -- MTX6
+x"1500", -- COM10
+x"58"&"10011110", -- MTXS 7 - autocontrast center enable
+x"3d"&"11000000", -- COM13 7 - gamma enable, 6 - UV sat lvl
+x"1714", -- HSTART
+x"1802", -- HSTOP
+x"3280", -- HREF
+x"1903", -- VSTRT
+x"1a7b", -- VSTOP
+x"030a", -- VREF
+x"0f"&"01000001", -- COM6 6 - reserved, 1 - reserved
+x"1e00", -- MVFP
+x"33"&"00001011", -- CHLF 70 - reserved
+x"3c"&"01000000", -- COM12 - 60 - reserved
+x"6900", -- GFIX
+x"7400", -- REG74
+x"703a", -- SCALING_XSC 60 - horizontal scale factor
+x"7135", -- SCALING_YSC 60 - vertical scale factor
+x"72"&"00100010", -- SCALING_DCWCTR 54 - v down sample 4, 10 - h down sample 4
+x"73"&"11110010", -- SCALING_PCLK_DIV 74 - reserved, 20 - clk dv dsp by 4
+x"a2"&"00000010", -- SCALING_PCLK_DELAY 60 - scaling output delay
+x"b0"&"10000100", -- RSVD
+x"b1"&"00001100", -- ABLC1 73 - reserved, 2 - ABLC enable, 10 - reserved
+x"b2"&"00001110", -- RSVD XX
+x"b3"&"10000000", -- THL_ST ABLC target
 x"ffff"
 );
 end package p_ov7670_rom;
