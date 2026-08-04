4fcf5067733832a0a00cfc77ce791a27d6b92021
kedziorno <kjoblak85@gmail.com>
2026-08-04 22:29:14 +0200

    cleanup

--- hdl/ov7670_registers.vhd
+++ hdl/ov7670_registers.vhd
@@ -1,126 +1,142 @@
 ----------------------------------------------------------------------------------
--- 'Command' contains the registers address (8 bit) and 
+-- 'Command' contains the registers address (8 bit) and
 -- the value assigned to those registers (8 bit). Both of them is concantenated.
--- View datasheet page 10 - 19.  
+-- View datasheet page 10 - 19.
 ----------------------------------------------------------------------------------
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.p_contants.all;
 use work.p_ov7670_rom.all;
 
 entity ov7670_registers is
-generic (constant MODE : integer := 0);
-    Port ( reset, clk : in  STD_LOGIC;
-           resend : in  STD_LOGIC;
-           advance : in  STD_LOGIC;
-           command : out  STD_LOGIC_VECTOR (15 downto 0);
-           done : out  STD_LOGIC);
-end ov7670_registers;
+generic (
+  c_module_mode : module_mode_st := c_module_mode_syn
+);
+port (
+  i_clock : in  std_logic;
+  i_reset : in  std_logic;
+  resend  : in  std_logic;
+  advance : in  std_logic;
+  command : out std_logic_vector (15 downto 0);
+  done    : out std_logic
+);
+end entity ov7670_registers;
 
 architecture raw_signal of ov7670_registers is
 
-  constant NC : integer := 24+1;
-  signal sequence : INTEGER range 0 to NC-1 := 0;
-  constant c_wait_reset : integer := 8193*100; -- to next busy_sr(31), wait ~16.5ms for reset
-  signal wait_reset : integer range 0 to c_wait_reset - 1;
-  signal cmd_reg : STD_LOGIC_VECTOR (15 downto 0);
+  constant c_nc         : integer := 24 + 1;
+  constant c_wait_reset : integer := 8193 * 100; -- to next busy_sr(31), wait ~16.5ms for reset
+  signal sequence       : integer range 0 to c_nc - 1;
+  signal wait_reset     : integer range 0 to c_wait_reset - 1;
+  signal cmd_reg        : std_logic_vector (15 downto 0);
 
 begin
 
   command <= cmd_reg;
-  with cmd_reg select done <= '1' when x"FFFF", '0' when others;
+  with cmd_reg select done <= '1' when x"ffff", '0' when others;
 
-  rom_case : process (sequence) is
+  p_rom_case : process (sequence) is
   begin
     case (sequence) is
       -- reset, not ideal but works
-      when 0 => cmd_reg <= x"1280";
-      when 1 => cmd_reg <= x"fffe";
-      when 2 => cmd_reg <= x"1280";
-      when 3 => cmd_reg <= x"fffe";
+      when 00 => cmd_reg <= x"1280";
+      when 01 => cmd_reg <= x"fffe";
+      when 02 => cmd_reg <= x"1280";
+      when 03 => cmd_reg <= x"fffe";
       -- configuration registers
-      when 4 => cmd_reg <= x"12"&"00000100"; -- CLKRC - internal p-s
-      when 5 => cmd_reg <= x"6b"&"11000000"; -- DBLV - ic x8
-      when 6 => cmd_reg <= x"11"&"00000001"; -- COM14, 4,2:0 hsync period
-      when 7 => cmd_reg <= x"3e"&"00000000"; -- COM7 - RGB selection
-      when 8 => cmd_reg <= x"3b"&"00000000"; -- COM11 - divide vsync
-      when 9 => cmd_reg <= x"40"&"11010000"; -- COM15 - out ran 255, RGB565
-      when 10 => cmd_reg <= x"8c"&"00000010"; -- RGB444 - enable, xRGB
+      when 04 => cmd_reg <= x"12"&"00000100"; -- clkrc - internal p-s
+      when 05 => cmd_reg <= x"6b"&"11000000"; -- dblv - ic x8
+      when 06 => cmd_reg <= x"11"&"00000001"; -- com14, 4,2:0 hsync period
+      when 07 => cmd_reg <= x"3e"&"00000000"; -- com7 - rgb selection
+      when 08 => cmd_reg <= x"3b"&"00000000"; -- com11 - divide vsync
+      when 09 => cmd_reg <= x"40"&"11010000"; -- com15 - out ran 255, rgb565
+      when 10 => cmd_reg <= x"8c"&"00000010"; -- rgb444 - enable, xrgb
       when 11 => cmd_reg <= x"0c"&"00000100";
-      when 12 => cmd_reg <= x"17"&x"14"; -- HSTART
-      when 13 => cmd_reg <= x"18"&x"02"; -- HSTOP
-      when 14 => cmd_reg <= x"32"&x"80"; -- HREF
-      when 15 => cmd_reg <= x"19"&x"03"; -- VSTART
-      when 16 => cmd_reg <= x"1a"&x"7b"; -- VSTOP
-      when 17 => cmd_reg <= x"03"&x"3a"; -- VREF
+      when 12 => cmd_reg <= x"17"&x"14"; -- hstart
+      when 13 => cmd_reg <= x"18"&x"02"; -- hstop
+      when 14 => cmd_reg <= x"32"&x"80"; -- href
+      when 15 => cmd_reg <= x"19"&x"03"; -- vstart
+      when 16 => cmd_reg <= x"1a"&x"7b"; -- vstop
+      when 17 => cmd_reg <= x"03"&x"3a"; -- vref
       when 18 => cmd_reg <= x"70"&x"3a";
       when 19 => cmd_reg <= x"71"&x"35";
       when 20 => cmd_reg <= x"72"&x"01";
-      when 21 => cmd_reg <= x"73"&x"01"; -- SCALING_PCLK_DIV
-      when 22 => cmd_reg <= x"3a"&"00000001"; -- TSLB auto window, 00 have pattern
-      when 23 => cmd_reg <= x"15"&"00000010"; -- COM10 - negate VSYNC
+      when 21 => cmd_reg <= x"73"&x"01"; -- scaling_pclk_div
+      when 22 => cmd_reg <= x"3a"&"00000001"; -- tslb auto window, 00 have pattern
+      when 23 => cmd_reg <= x"15"&"00000010"; -- com10 - negate vsync
       when 24 => cmd_reg <= x"b0"&"10001000";
       when others => cmd_reg <= x"ffff";
     end case;
-  end process rom_case;
+  end process p_rom_case;
 
-  sequence_proc : process (clk) begin
-    if rising_edge(clk) then
-    if (reset = '1') then
-      sequence <= 0;
-      wait_reset <= 0;
-    else
-      if (cmd_reg = x"fffe") then
-        if (wait_reset = c_wait_reset - 1) then
-          wait_reset <= 0;
-          sequence <= sequence + 1;
-        else
-          wait_reset <= wait_reset + 1;
+  p_sequence_proc : process (i_clock) begin
+    if (rising_edge (i_clock)) then
+      if (i_reset = '1') then
+        sequence   <= 0;
+        wait_reset <= 0;
+      else
+        if (c_module_mode = c_module_mode_syn) then
+          if (cmd_reg = x"fffe") then
+            if (wait_reset = c_wait_reset - 1) then
+              wait_reset <= 0;
+              sequence   <= sequence + 1;
+            else
+              wait_reset <= wait_reset + 1;
+            end if;
+          elsif (resend = '1') then
+            sequence <= 0;
+          elsif (advance = '1') then
+            sequence <= sequence + 1;
+          end if;
         end if;
-      elsif resend = '1' then
-        sequence <= 0;
-      elsif advance = '1' then
-        sequence <= sequence + 1;
       end if;
     end if;
-    end if;
-  end process sequence_proc;
+  end process p_sequence_proc;
 
 end architecture raw_signal;
 
 --
 
-architecture Behavioral of ov7670_registers is
-  signal cmd_reg : STD_LOGIC_VECTOR (15 downto 0);
-  signal sequence : INTEGER range 0 to NC-1 := 0;
-  constant c_wait_reset : integer := 8193*100; -- to next busy_sr(31), wait ~16.5ms for reset
-  signal wait_reset : integer range 0 to c_wait_reset - 1;
+architecture behavioral of ov7670_registers is
+
+  constant c_wait_reset : integer := 8193 * 100; -- to next busy_sr(31), wait ~16.5ms for reset
+  signal sequence       : integer range 0 to c_nc - 1;
+  signal wait_reset     : integer range 0 to c_wait_reset - 1;
+  signal cmd_reg        : std_logic_vector (15 downto 0);
+
 begin
+
   command <= cmd_reg;
-  with cmd_reg select done <= '1' when x"FFFF", '0' when others;
-  cmd_reg <= ov7670_rom (sequence);
-  sequence_proc : process (clk) begin
-    if rising_edge(clk) then
-    if (reset = '1') then
-      sequence <= 0;
-      wait_reset <= 0;
+  with cmd_reg select done <= '1' when x"ffff", '0' when others;
+  cmd_reg <= ov7670_rom (sequence); -- from package p_ov7670_rom
+
+  p_sequence_proc : process (i_clock) begin
+    if (rising_edge (i_clock)) then
+      if (i_reset = '1') then
+        sequence   <= 0;
+        wait_reset <= 0;
       else
-      if (cmd_reg = x"fffe") then
-        if (wait_reset = c_wait_reset - 1) then
-          wait_reset <= 0;
-          sequence <= sequence + 1;
-        else
-          wait_reset <= wait_reset + 1;
+        if (c_module_mode = c_module_mode_syn) then
+          if (cmd_reg = x"fffe") then
+            if (wait_reset = c_wait_reset - 1) then
+              wait_reset <= 0;
+              sequence <= sequence + 1;
+            else
+              wait_reset <= wait_reset + 1;
+            end if;
+          elsif resend = '1' then
+            sequence <= 0;
+          elsif advance = '1' then
+            sequence <= sequence + 1;
+          end if;
         end if;
-      elsif resend = '1' then
-        sequence <= 0;
-      elsif advance = '1' then
-        sequence <= sequence + 1;
       end if;
     end if;
-    end if;
-  end process sequence_proc;
+  end process p_sequence_proc;
 
-end Behavioral;
+end architecture behavioral;
 
--- hdl/ov7670_rom.vhd
+++ hdl/ov7670_rom.vhd
@@ -1,61 +1,62 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 package p_ov7670_rom is
-  constant NC : integer := 56-10+1;
-  type cmd_rom is array (0 to NC-1) of STD_LOGIC_VECTOR (15 downto 0);
+  constant c_nc : integer := 56 - 10 + 1; -- line number with x"ffff"
+  type cmd_rom is array (0 to c_nc - 1) of std_logic_vector (15 downto 0);
 
   constant ov7670_rom : cmd_rom :=
-(
-x"1280", -- COM7   Reset -- Do it twice to make sure its wiped
-x"1280", -- COM7   Reset -- choose output format. 
-x"fffe",
-x"12"&"00000100", -- COM7 2 - RGB
-x"11"&"10000001", -- CLKRC 7 - reserved
-x"6b"&"01000000", -- DBLV
-x"8c"&"00000010", -- RGB444 xRGB
-x"0c"&"00000000", -- COM3
-x"3e"&"00010001", -- COM14 4 - DCW and scaling PCLK, 20 - Divided by 2
-x"04"&"00000000", -- COM1
-x"40"&"11011000", -- COM15 76 - FF, 54 - RGB555
-x"13"&"00011111", -- COM8 43 - reserved, 2 - AGC, 1 - AWB, 0 - AEC
-x"41"&"00001000", -- COM16 3 - AWB gain
-x"3a"&"00000001", -- TSLB 2 - reserved
-x"14"&"00011000", -- COM9 4 - AGC 4x, 3 - reserved
-x"4fb3", -- MTX1
-x"50b3", -- MTX2
-x"5100", -- MTX3
-x"523d", -- MTX4
-x"53a7", -- MTX5
-x"54e4", -- MTX6
-x"58"&"10011110", -- MTXS 7 - autocontrast center enable
-x"1500", -- COM10
-x"3d"&"11000000", -- COM13 7 - gamma enable, 6 - UV sat lvl
-x"1711", -- HSTART
-x"1861", -- HSTOP
-x"3280", -- HREF
-x"1903", -- VSTRT
-x"1a7b", -- VSTOP
-x"00"&"01100000", -- GAIN
-x"03"&"00000000", -- VREF
-x"0f"&"01000001", -- COM6 6 - reserved, 1 - reserved
-x"1e"&"00100000", -- MVFP
-x"33"&"00001011", -- CHLF 70 - reserved
-x"3c"&"01000000", -- COM12 - 60 - reserved
-x"6900", -- GFIX
-x"7400", -- REG74
-x"703a", -- SCALING_XSC 60 - horizontal scale factor
-x"7135", -- SCALING_YSC 60 - vertical scale factor
-x"72"&"00100010", -- SCALING_DCWCTR 54 - v down sample 4, 10 - h down sample 4
-x"73"&"11110010", -- SCALING_PCLK_DIV 74 - reserved, 20 - clk dv dsp by 4
-x"a2"&"00000010", -- SCALING_PCLK_DELAY 60 - scaling output delay
-x"b0"&"10000100", -- RSVD
-x"b1"&"00001100", -- ABLC1 73 - reserved, 2 - ABLC enable, 10 - reserved
-x"b2"&"00001110", -- RSVD XX
-x"b3"&"10000000", -- THL_ST ABLC target
-x"ffff"
-);
+  (
+    x"1280", -- COM7   Reset -- Do it twice to make sure its wiped
+    x"1280", -- COM7   Reset -- choose output format.
+    x"fffe", -- PASUE
+    x"12"&"00000100", -- COM7 2 - RGB
+    x"11"&"10000001", -- CLKRC 7 - reserved
+    x"6b"&"01000000", -- DBLV
+    x"8c"&"00000010", -- RGB444 xRGB
+    x"0c"&"00000000", -- COM3
+    x"3e"&"00010001", -- COM14 4 - DCW and scaling PCLK, 20 - Divided by 2
+    x"04"&"00000000", -- COM1
+    x"40"&"11011000", -- COM15 76 - FF, 54 - RGB555
+    x"13"&"00011111", -- COM8 43 - reserved, 2 - AGC, 1 - AWB, 0 - AEC
+    x"41"&"00001000", -- COM16 3 - AWB gain
+    x"3a"&"00000001", -- TSLB 2 - reserved
+    x"14"&"00011000", -- COM9 4 - AGC 4x, 3 - reserved
+    x"4fb3", -- MTX1
+    x"50b3", -- MTX2
+    x"5100", -- MTX3
+    x"523d", -- MTX4
+    x"53a7", -- MTX5
+    x"54e4", -- MTX6
+    x"58"&"10011110", -- MTXS 7 - autocontrast center enable
+    x"1500", -- COM10
+    x"3d"&"11000000", -- COM13 7 - gamma enable, 6 - UV sat lvl
+    x"1711", -- HSTART
+    x"1861", -- HSTOP
+    x"3280", -- HREF
+    x"1903", -- VSTRT
+    x"1a7b", -- VSTOP
+    x"00"&"01100000", -- GAIN
+    x"03"&"00000000", -- VREF
+    x"0f"&"01000001", -- COM6 6 - reserved, 1 - reserved
+    x"1e"&"00100000", -- MVFP
+    x"33"&"00001011", -- CHLF 70 - reserved
+    x"3c"&"01000000", -- COM12 - 60 - reserved
+    x"6900", -- GFIX
+    x"7400", -- REG74
+    x"703a", -- SCALING_XSC 60 - horizontal scale factor
+    x"7135", -- SCALING_YSC 60 - vertical scale factor
+    x"72"&"00100010", -- SCALING_DCWCTR 54 - v down sample 4, 10 - h down sample 4
+    x"73"&"11110010", -- SCALING_PCLK_DIV 74 - reserved, 20 - clk dv dsp by 4
+    x"a2"&"00000010", -- SCALING_PCLK_DELAY 60 - scaling output delay
+    x"b0"&"10000100", -- RSVD
+    x"b1"&"00001100", -- ABLC1 73 - reserved, 2 - ABLC enable, 10 - reserved
+    x"b2"&"00001110", -- RSVD XX
+    x"b3"&"10000000", -- THL_ST ABLC target
+    x"ffff" -- END
+  );
 end package p_ov7670_rom;
 
 package body p_ov7670_rom is
 end package body p_ov7670_rom;
+
